aada91a32116ba858c5c4a7f6e2352554655d20a
Zachary McGrath <zacharymcgrath@Zacharys-MacBook-Pro.local>
2019-04-14 19:23:50 -0500

    made squaring and pickling two different operations in its own folder and pickled dog images

---
+++ square_and_pickle_pics.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import pickle
+import cv2
+
+def square(path, img_size, color):
+	try:
+		img = cv2.imread(path)
+		# size is in (height, width) format
+		old_size = img.shape[:2] 
+
+		ratio = float(img_size)/max(old_size)
+		new_size = tuple([int(x*ratio) for x in old_size])
+
+		# new_size should be in (width, height) format
+
+		img = cv2.resize(img, (new_size[1], new_size[0]))
+
+		delta_w = img_size - new_size[1]
+		delta_h = img_size - new_size[0]
+		top, bottom = delta_h//2, delta_h-(delta_h//2)
+		left, right = delta_w//2, delta_w-(delta_w//2)
+		return (path ,cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color))
+	
+	except Exception as e:
+		print('Could not open image: ' + str(path))
+
+def pickle_pics(pic_list, pickle_path):
+	if 'list' not in str(type(pic_list)):
+		print('Invalid parameter: first parameter must be list of np arrays')
+		return
+	for t in pic_list:
+		try:
+			if t is None:
+				continue
+			path, data = t
+			name = pickle_path + '/'.join(path.split('/')[-2:-1]).split('.')[0] + '.pickle'
+			np.save(name, data)
+		except Exception as e:
+			print(e)
--- training.py
+++ training.py
@@ -1,39 +1,16 @@
 import sys, os 
 from os import walk
 import numpy as np
-import cv2
-import pickle
+from square_and_pickle_pics import *
 
 # Params for training on a data set
 training_folder = 'training_images/'
+pickled_pics = {'simpsons': training_folder + '/simpsons/pickled_images/', 'dogs': training_folder + 'dogs/pickled_images/'}
 images = {'simpsons': training_folder + 'simpsons/simpsons_dataset/', 'dogs': training_folder + 'dogs/images/'}
 annotations = {'simpsons': training_folder + 'simpsons/annotation.txt', 'dogs': training_folder + 'annotation/'}
 ouptut_folder = 'trained/'
 img_size = 250
 color = [0, 0, 0]
-
-def block_imgs(path):
-	try:
-		img = cv2.imread(path)
-		# size is in (height, width) format
-		old_size = img.shape[:2] 
-
-		ratio = float(img_size)/max(old_size)
-		new_size = tuple([int(x*ratio) for x in old_size])
-
-		# new_size should be in (width, height) format
-
-		img = cv2.resize(img, (new_size[1], new_size[0]))
-
-		delta_w = img_size - new_size[1]
-		delta_h = img_size - new_size[0]
-		top, bottom = delta_h//2, delta_h-(delta_h//2)
-		left, right = delta_w//2, delta_w-(delta_w//2)
-
-		return (path ,cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color))
-	
-	except Exception as e:
-		print('Could not open image: ' + str(path))
 
 
 def train(s):
@@ -44,30 +21,19 @@ def train(s):
 
 	num_output = len(os.listdir(images[s]))
 	out_f = ouptut_folder + s + '.pickle'
-
-	sqr_imgs = []
-	if 'simpsons' in s: 
-		#cache the blocked images
-		pickle_path = 'training_images/simpsons/pickled_images/'
+	pickled_path = pickled_pics[s]
+	np_pic_list = []
+	
+	if len(os.listdir(pickled_path)) == 0:
+		print('Normalizing photos to {}px square images...'.format(img_size))
 		for i in imgs:
-			if not os.path.isfile(i):
-				continue
-			sqr_imgs.append(block_imgs(i))
-
-		print('pickling square images')
-		
-		for t in sqr_imgs:
-			try:
-				if t is None:
-					continue
-				path, data = t
-				name = pickle_path + '/'.join(path.split('/')[-2:-1]).split('.')[0] + '.pickle'
-				np.save(name, data)
-			except Exception as e:
-				print('failed with: ' + str(t))
+			np_pic_list.append(square(i, img_size, color))
+		print('Done\nPickling images...')
+		pickle_pics(np_pic_list, pickled_path)
+		print('Done')
 
 	else:
-		print('TODO: Annotations for dogs')
+		print('TODO: LOAD IN PICKLED IMAGES FOR TRAINING')
 
 
 def main(args):
